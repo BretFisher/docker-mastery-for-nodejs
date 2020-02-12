c7ef698fc6067390246dc7991591c1cc1498fe34
Bret Fisher <bret@bretfisher.com>
2020-02-11 19:08:55 -0500

    replaced sample-strapi with new example

--- sample-strapi/config/functions/cron.js
+++ sample-strapi/config/functions/cron.js
@@ -5,16 +5,16 @@
  * to run scheduled jobs.
  *
  * The cron format consists of:
- * [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
+ * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
+ *
+ * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#cron-tasks
  */
 
 module.exports = {
-
   /**
    * Simple example.
    * Every monday at 1am.
    */
-
   // '0 1 * * 1': () => {
   //
   // }
