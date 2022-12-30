159d23418f098a68bf4746d7f4673668acaf47c4
Bret Fisher <bret@bretfisher.com>
2022-12-30 12:35:35 -0500

    update ultimate for 2023

--- ultimate-node-dockerfile/server.js
+++ ultimate-node-dockerfile/server.js
@@ -1,9 +1,7 @@
-/* jshint node: true */
-/* jshint esversion: 6 */
 var express = require('express'),
     async = require('async'),
     pg = require('pg'),
-    {Pool} = require('pg'),
+    { Pool } = require('pg'),
     path = require('path'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
@@ -14,11 +12,7 @@ var express = require('express'),
 
 io.set('transports', ['polling']);
 
-var port = process.env.PORT || 80;
-
-var pool = new pg.Pool({
-  connectionString: 'postgres://postgres@db/postgres'
-});
+var port = process.env.PORT || 4000;
 
 io.sockets.on('connection', function (socket) {
 
@@ -29,21 +23,25 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
+var pool = new pg.Pool({
+  connectionString: 'postgres://postgres:postgres@db/postgres'
+});
+
 async.retry(
   {times: 1000, interval: 1000},
   function(callback) {
     pool.connect(function(err, client, done) {
       if (err) {
-        console.error('Waiting for db');
+        console.error("Waiting for db");
       }
       callback(err, client);
     });
   },
   function(err, client) {
     if (err) {
-      return console.error('Giving up');
+      return console.error("Giving up");
     }
-    console.log('Connected to db');
+    console.log("Connected to db");
     getVotes(client);
   }
 );
@@ -51,13 +49,13 @@ async.retry(
 function getVotes(client) {
   client.query('SELECT vote, COUNT(id) AS count FROM votes GROUP BY vote', [], function(err, result) {
     if (err) {
-      console.error('Error performing query: ' + err);
+      console.error("Error performing query: " + err);
     } else {
       var votes = collectVotesFromResult(result);
-      io.sockets.emit('scores', JSON.stringify(votes));
+      io.sockets.emit("scores", JSON.stringify(votes));
     }
 
-    setTimeout(function() {getVotes(client); }, 1000);
+    setTimeout(function() {getVotes(client) }, 1000);
   });
 }
 
@@ -75,9 +73,9 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   next();
 });
 
